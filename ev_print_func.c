bf828f11dba68d876c70004d618511257fff00c4
Akinwale Akinjiola <akinjil@gmail.com>
2023-01-23 22:06:25 +0000

    Updated ev_print_func.c with print_prg

--- ev_print_func.c
+++ ev_print_func.c
@@ -10,7 +10,8 @@
 int ev_print_func(const char *s, int index)
 {
 	print_t pr[] = {
-		{"c", print_chr}, {"s", print_str}, {NULL, NULL}
+		{"c", print_chr}, {"s", print_str},
+		{"%", print_prg}, {NULL, NULL}
 	};
 	int i = 0, j = 0, first_index;
 
