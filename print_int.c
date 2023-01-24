a26045934f4fdfed5d088ad236a5f7c349a32855
Akinwale Akinjiola <akinjil@gmail.com>
2023-01-24 20:37:58 +0000

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -41,3 +41,36 @@ int print_int(va_list args, char *buf, unsigned int ibuf)
 	}
 	return (i + isneg);
 }
+
+/**
+ * print_unint - prints an integer
+ * @args: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+
+int print_unint(va_list args, char *buf, unsigned int ibuf)
+{
+	int int_input;
+	unsigned int int_in, int_temp, i, div, isneg;
+
+	int_input = va_arg(args, unsigned int);
+	isneg = 0;
+	int_in = int_input;
+
+	int_temp = int_in;
+	div = 1;
+
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i + isneg);
+}
